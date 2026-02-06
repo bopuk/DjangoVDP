31d347fb7ce53dd26a62c033a79dfec4b68c955f
kabinet303 <kabinet303@localhost.localdomain>
2026-02-06 12:45:32 +0300

    Создание проекта

---
+++ mainVDP/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MainvdpConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mainVDP'
---
+++ mainVDP/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def mainVDP (request):
+    return HttpResponse('app mainVDP')
+# Create your views here.
